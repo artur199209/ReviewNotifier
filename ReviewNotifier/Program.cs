68779375761ad7893665ffa381c19950ba2efbb3
Grzegorz Slazak <gregorysl@gmail.com>
2019-02-15 14:41:11 +0100

    cleanup

--- ReviewNotifier/Program.cs
+++ ReviewNotifier/Program.cs
@@ -8,24 +8,22 @@ namespace ReviewNotifier
 {
     class Program
     {
-        private static Settings _settings;
         private static LastIdSettings _lastIdSettings;
         private static TeamsNotifier _teams;
-        private static LoginBuilder _loginBuilder;
         private static TfsDataConnector _tfs;
-        private static Timer timer;
         private static int _lastId;
 
         static void Main(string[] args)
         {
-            _settings = new Config().GetSettings();
+            var settings = new Config().GetSettings();
+            var loginBuilder = new LoginBuilder(settings);
+
             _lastIdSettings = new LastIdSettings();
             _lastId = _lastIdSettings.Get();
-            _teams = new TeamsNotifier(_settings.WebHookUrl);
-            _loginBuilder = new LoginBuilder(_settings);
-            _tfs = new TfsDataConnector(_settings, _loginBuilder, _lastId);
+            _teams = new TeamsNotifier(settings.WebHookUrl);
+            _tfs = new TfsDataConnector(settings, loginBuilder, _lastId);
 
-            timer = new Timer
+            var timer = new Timer
             {
                 AutoReset = true,
                 Interval = 60000
