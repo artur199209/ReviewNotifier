1a9953d089bf6323632c4c08c7737b04d0323ac1
artur199209 <arti190@gmail.com>
2019-02-20 12:14:12 +0100

    Create Logger.cs

---
+++ ReviewNotifier/Helpers/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ReviewNotifier.Helpers
+{
+    public static class Logger
+    {
+        private static readonly string Path = "Logs/logs.txt".FullFileLocation();
+
+        public static void SaveLog(string messsage)
+        {
+            try
+            {
+                Directory.CreateDirectory("Logs/".FullFileLocation());
+                File.AppendAllText(Path, messsage);
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+        }
+    }
+}
