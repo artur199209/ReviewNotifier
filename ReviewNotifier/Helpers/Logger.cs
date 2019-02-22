48bca5e5d8eea176de57e30673e32f831b18aa3d
artur199209 <arti190@gmail.com>
2019-02-22 13:40:55 +0100

    Rename method and include Console.Writeline inside

--- ReviewNotifier/Helpers/Logger.cs
+++ ReviewNotifier/Helpers/Logger.cs
@@ -7,12 +7,13 @@ namespace ReviewNotifier.Helpers
     {
         private static readonly string Path = "Logs/logs.txt".FullFileLocation();
 
-        public static void SaveLog(string messsage)
+        public static void Write(string message)
         {
             try
             {
                 Directory.CreateDirectory("Logs/".FullFileLocation());
-                File.AppendAllText(Path, messsage +  Environment.NewLine);
+                File.AppendAllText(Path, message +  Environment.NewLine);
+                Console.WriteLine(message);
             }
             catch (Exception ex)
             {
