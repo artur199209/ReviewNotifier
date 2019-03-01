0e2c7598fe4a28960e7462da8e021de9f5d0bf34
artur199209 <arti190@gmail.com>
2019-03-01 12:12:00 +0100

    Refactor Log4NetConfig class

--- ReviewNotifier/Log4NetConfig.cs
+++ ReviewNotifier/Log4NetConfig.cs
@@ -1,27 +1,13 @@
-﻿using System.IO;
-using System.Reflection;
-using System.Xml;
+﻿using System.Reflection;
 using log4net;
-using ReviewNotifier.Interfaces;
 
 namespace ReviewNotifier
 {
-    public class Log4NetConfig : ILog4NetConfig
-    {
-        private static ILog _log;
-
-        public Log4NetConfig()
+    public class Log4NetConfig
+    { 
+        public static ILog GetLogger()
         {
-            XmlDocument log4NetConfig = new XmlDocument();
-            log4NetConfig.Load(File.OpenRead("log4net.config"));
-            var repo = LogManager.CreateRepository(Assembly.GetEntryAssembly(),
-                typeof(log4net.Repository.Hierarchy.Hierarchy));
-            log4net.Config.XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
-            _log = LogManager.GetLogger(typeof(Program));
+            return LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         }
-
-        public void LogErrorMessage(string message) => _log.Error(message);
-
-        public void LogInfoMessage(string message) => _log.Info(message);
     }
 }
