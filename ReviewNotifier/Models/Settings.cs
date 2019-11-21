4d89a6bdf0a805cc26d5b63f3bab64ba8dd71359
gregorysl <gregorysl@gmail.com>
2019-11-21 15:24:18 +0100

    Additional settings

--- ReviewNotifier/Models/Settings.cs
+++ ReviewNotifier/Models/Settings.cs
@@ -7,6 +7,8 @@ namespace ReviewNotifier.Models
         public string WebHookUrl { get; set; }
         public string TfsUrl { get; set; }
         public string PersonalAccessTokenToTFS { get; set; }
+        public string AdoUrl { get; set; }
+        public string AdoToken { get; set; }
         public int CodeReviewCount { get; set; }
         public List<string> Developers { get; set; }
     }
